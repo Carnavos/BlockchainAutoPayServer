bfcacbd97178173424b6b49019ea0bc64c65c012
Theo Carnavos <tcarnavos@gmail.com>
2016-06-14 21:09:10 -0500

    add AspNet Authentication dependencies to project.json and AddAuthentication to Startup.cs

--- BlockchainAutoPay/src/BlockchainAutoPay/Startup.cs
+++ BlockchainAutoPay/src/BlockchainAutoPay/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using BlockchainAutoPay.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace BlockchainAutoPay
 {
@@ -36,6 +37,10 @@ namespace BlockchainAutoPay
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Authentication services
+            services.AddAuthentication(
+                options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme); 
+
             var connection = @"Server=(localdb)\mssqllocaldb;Database=BCAPDB;Trusted_Connection=True;";
             services.AddDbContext<BCAPContext>(options => options.UseSqlServer(connection));
 
